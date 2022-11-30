983e6e61ff885d813eaf4bd5b31cf8f7e16cd7ac
Alvin Quach <alvinwquach@gmail.com>
2022-11-30 15:35:59 -0800

    Create layout component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,14 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
-import Footer from "../components/common/Footer";
-import Navbar from "../components/common/Navbar";
 
 const Home: NextPage = () => {
   return (
     <>
-      <Navbar />
       <div className="flex min-h-screen flex-col items-center justify-center py-2">
         <Head>
           <title>Rick and Morty - C137</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
-
         <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
           <h1 className="text-6xl font-bold">
             Welcome to{" "}
@@ -75,23 +70,6 @@ const Home: NextPage = () => {
             </a>
           </div>
         </main>
-        <Footer />
-        {/* <footer className="flex h-24 w-full items-center justify-center border-t">
-          <a
-            className="flex items-center justify-center gap-2"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by the Citadel of Ricks
-            <Image
-              src="/logo/RickAndMorty.jpeg"
-              alt="Vercel Logo"
-              width={72}
-              height={16}
-            />
-          </a>
-        </footer> */}
       </div>
     </>
   );
