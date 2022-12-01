26c2128bcbd58914872a66fa5155718108a610e4
Alvin Quach <alvinwquach@gmail.com>
2022-11-30 16:10:01 -0800

    Optimize Google Font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,21 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/common/Layout";
+import { Roboto } from "@next/font/google";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-roboto",
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <main className={`${roboto.variable} font-sans`}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </main>
   );
 }
 
